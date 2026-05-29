017df27ae8e296a3e76202670418b1f65dc62f9a
carla <guiasastrologicas@gmail.com>
2026-05-29 10:33:51 -0500

    test 5

--- foundations/05_joinStrings/joinStrings-example.js
+++ foundations/05_joinStrings/joinStrings-example.js
@@ -3,14 +3,19 @@ const thisYear = 1965;
 const firstName = "Carlos";
 const lastName = "Stevenson";
 
-const greeting = "Hello! My name is " + firstName + " " + lastName + " and I am " + (thisYear - birthYear) + " years old.";
+const fullName = firstName + lastName;
+const age = 18;
+
+const greeting = 'Hello! My name is' + fullName + 'and I am'+ age+ 'years old';
 
 console.log(greeting);
+
 
 module.exports = {
   greeting,
   birthYear,
   thisYear,
   firstName,
-  lastName
+  lastName,
+  fullName
 }
