a523fa6818a92688d05495387f368bd65c2b0fbe
carla <guiasastrologicas@gmail.com>
2026-06-05 18:58:59 -0500

    repeat string

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,6 +1,20 @@
-const repeatString = function() {
+const repeatString = function(word, number) {
 
-};
+    let result = "";
+
+      if(number<0){
+            return "ERROR";
+        }else{
+
+    for (let i=0; i<number;i++){
+
+        result += word;
+    }
+
+    }
+
+    return result;
+}
 
 // Do not edit below this line
 module.exports = repeatString;
