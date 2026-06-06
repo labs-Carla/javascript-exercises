f0f40d23aeb7f4c75e9da12aa91fbe6ec68885d0
carla <guiasastrologicas@gmail.com>
2026-06-05 19:16:44 -0500

    reverse string

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,4 +1,12 @@
-const reverseString = function() {
+const reverseString = function(str) {
+
+    let result = "";
+
+    for(let i=str.length-1;i>=0 ;i--){
+        result = result + str[i];
+    }
+
+    return result;
 
 };
 
