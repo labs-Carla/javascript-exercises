93d776569ce0f3efbe9bc2cb5f17edca5709e841
carla <guiasastrologicas@gmail.com>
2026-06-07 10:31:37 -0500

    sumAll

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,4 +1,16 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, number) {
+
+    let result = [];
+
+    for (let i=0; i<arr.length; i++){
+
+        if(arr[i] !== number){
+            result.push(arr[i]);
+        }
+    }
+
+    return result;
+
 };
 
 // Do not edit below this line
--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,16 @@
-const sumAll = function() {
+const sumAll = function(inicio, fin) {
 
+    if(inicio>fin){
+    let temp = inicio;
+    inicio = fin;
+    fin = temp;
+    }
+    let suma = 0;
+
+   for (let i=inicio; i<=fin;i++){
+    suma += i;
+   }
+   return suma;
 };
 
 // Do not edit below this line
